c10aec45f18b53d7d046f2c87c412bb8fa2be188
Vladimir <tchaikovski@mail.ru>
2021-02-26 22:27:17 +0300

    Прописаны шаблоны

--- shop/models.py
+++ shop/models.py
@@ -1,34 +1,43 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Имя категории')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='url')
 #  TODO: добавить тайтл, дескрипшин, изображение
 
     class Meta:
         ordering = ('name',)
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Наименованеи')
+    slug = models.SlugField(max_length=200, db_index=True, verbose_name='url')
+    image = models.ImageField(upload_to='product/%Y/%m/%d/', blank=True, verbose_name='Изображение')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    available = models.BooleanField(default=True, verbose_name='Наличие')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
 
     class Meta:
         ordering = ('name',)
         index_together = (('id', 'slug'),)
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
